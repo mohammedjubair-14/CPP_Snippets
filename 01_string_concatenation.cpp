3cfbdf61642986def0ef0a7ea27ff263db7ac286
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-06-26 21:59:45 +0530

    Add files via upload

---
+++ 01_string_concatenation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string FirstName="Mohammed";
+    string LastName="Jubair";
+    string FullName=FirstName+" "+LastName;
+    cout<<FullName<<endl;
+
+    return 0;
+}
+
+
+  /*  string FirstName="Mohammed";
+    string LastName="Jubair";
+    string FullName=FirstName.append(LastName);
+    cout<<FullName<<endl;  */
+
+// using constructor
+
+  /*string a("Mohammed Jubair");
+cout<<a<<endl;
+return 0;
+}
+
