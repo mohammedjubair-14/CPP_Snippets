3a334f7019f2a45b902fb9e8fb5a30e267cfd6a8
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-06-26 21:58:58 +0530

    Add files via upload

---
+++ 01_string_Access.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    string name="Mohammed Jubair";
+    cout<<name[0];endl;
+    name[0]='z';
+    cout<<name;endl;
+
+
+
+    return 0;
+}
