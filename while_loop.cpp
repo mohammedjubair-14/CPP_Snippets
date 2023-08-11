f495f25d458e6c9788425d2a4ae1f15035d8eca3
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-08-11 21:28:44 +0530

    Add files via upload

---
+++ while_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1,n;
+    cout<<"Enter the limit"<<endl;
+    cin>>n;
+    while(i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+
+
+    }
+    return 0;
+}
